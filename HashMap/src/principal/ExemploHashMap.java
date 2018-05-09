cee67fd0ffefecb558bd407b7472ff0cca95f560
monaly <monalyvital.ufal@gmail.com>
2018-05-09 16:50:16 -0300

    regras de inferencia

--- HashMap/src/principal/ExemploHashMap.java
+++ HashMap/src/principal/ExemploHashMap.java
@@ -11,9 +11,16 @@ public class ExemploHashMap {
 		
 		//op.insert("Invalido ", 1, 60); // 1 - verdadeiro
 		//op.insert("cansacod", 1, 60); // 0 - falso
+		System.out.println("Será que você pode doar sangue?");
+		System.out.println(" ");
+		op.inserirRegra("gravidez ou amamenta ", "Você não pode doar sangue", 100);
+		op.inserirRegra("bebeu ou gravidez ou tem_alguma_DST", "Continua sem poder doar", 90);
+		op.inserirRegra("fez_tatuagem_6meses ou gravidez ou tem_alguma_DST ou amamenta", " Espere algum tempo para poder doar", 90);
+		op.inserirRegra("maior_de10Anos e tem_Hepatite ou fez_tatuagem_6meses ou gravidez ou tem_alguma_DST ou amamenta", " Você continua impedido de doar", 90);
+		op.inserirRegra("possui_malaria ou maior_de10Anos e tem_Hepatite ou fez_tatuagem_6meses ou gravidez ou tem_alguma_DST ou amamenta", " Espere algum tempo para poder doar", 90);
+		op.inserirRegra("maior_de_idade e peso_maior_50", " Doe Sangue, salve vidas", 90);
+		op.inserirRegra("maior_de_idade e peso_maior_50 e esta_alimentado", " Procure o posto de coleta mais próximo", 90);
 		
-		op.inserirRegra("cansaco e sono", "dormir", 100);
-		op.inserirRegra("sono e fome", "Deus lhe ajude nesta hora dificil", 90);
 		
 		tr.start();
 		
