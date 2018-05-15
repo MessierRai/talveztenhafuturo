098343ab6141f9a2ba9891c112d2c1afe0a6f7e6
Raí <rai.lima17@live.com>
2018-05-15 20:09:40 -0300

    add regra

--- HashMap/src/principal/ExemploHashMap.java
+++ HashMap/src/principal/ExemploHashMap.java
@@ -21,15 +21,19 @@ public class ExemploHashMap {
 		op.inserirRegra("possui_malaria ou maior_de10Anos e tem_Hepatite ou fez_tatuagem_6meses ou gravidez ou tem_alguma_DST ou amamenta", " Espere algum tempo para poder doar", 90);
 		op.inserirRegra("maior_de_idade e peso_maior_50", " Doe Sangue, salve vidas", 90);
 		//op.inserirRegra("maior_de_idade e peso_maior_50 e esta_alimentado", "pode_doar", 90);
-		*/
+		
 		op.inserirRegra("R e E ou J", "A", 100);
 		op.inserirRegra("A e T", "B", 100);
 		op.inserirRegra("A e B ou Y", "C", 100);
 		op.inserirRegra("B e C", "D", 100);
 		op.inserirRegra("D ou A", "F", 100);
+		*/
+		
+		op.inserirRegra("nao_gravidez e nao_amamenta e nao_bebeu e nao_tem_DST e nao_fez_tatuagem_6meses e nao_tem_hepatite e nao_possui_malaria", "sem_restricao", 100);
+		op.inserirRegra("sem_restricao e maior_de_idade e peso_maior_50 e esta_alimentado", "pode_doar", 100);
 		
 				
-		tr.start("F");
+		tr.start("pode_doar");
 		
 		
 		
